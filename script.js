0999141e4fe39b716ecdd924473456f31706ceb4
Xanddev77 <xavieralexandre985@gmail.com>
2026-02-15 18:22:52 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -298,35 +298,3 @@ gsap.timeline({ repeat: -1 })
       ease: "power4.out"
     }
   );
-/* ===============================
-   EFEITO 3D NOS PROJETOS
-================================ */
-
-document.querySelectorAll(".project-card").forEach(card => {
-  card.addEventListener("mousemove", e => {
-    const rect = card.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-
-    const rotateX = -(y - centerY) / 12;
-    const rotateY = (x - centerX) / 12;
-
-    card.style.transform = `
-      rotateX(${rotateX}deg)
-      rotateY(${rotateY}deg)
-      translateZ(20px)
-    `;
-  });
-
-  card.addEventListener("mouseleave", () => {
-    card.style.transform = `
-      rotateX(0deg)
-      rotateY(0deg)
-      translateZ(0)
-    `;
-  });
-});
